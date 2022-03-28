3efbd23eee091c2f390115b7acc0bdc38f99fa39
Mads Overgård Henningsen <au648318@uni.au.dk>
2022-03-28 11:01:35 +0200

    Got Coq working and WIP proofs for secp256k1

--- coq/src/Hacspec_Secp256k1.v
+++ coq/src/Hacspec_Secp256k1.v
@@ -1,11 +1,13 @@
 (** This file was automatically generated using Hacspec **)
-Require Import Lib MachineIntegers.
+Require Import Hacspec_Lib MachineIntegers.
 From Coq Require Import ZArith.
 Import List.ListNotations.
 Open Scope Z_scope.
 Open Scope bool_scope.
 Open Scope hacspec_scope.
-Require Import Hacspec.Lib.
+Require Import Hacspec_Lib.
+Import Bool.
+Import GZnZ.
 
 Definition field_canvas_t := nseq (int8) (32).
 Definition secp256k1_field_element_t :=
@@ -23,6 +25,12 @@ Notation "'affine_t'" := ((
   secp256k1_field_element_t
 )) : hacspec_scope.
 
+Definition infinity  : affine_t :=
+  (nat_mod_one , nat_mod_zero ).
+
+Definition is_infinity (p_3 : affine_t) : bool :=
+  (p_3) =.? (infinity ).
+
 Definition is_point_on_curve (p_0 : affine_t) : bool :=
   let '(x_1, y_2) :=
     p_0 in 
@@ -30,12 +38,6 @@ Definition is_point_on_curve (p_0 : affine_t) : bool :=
         nat_mod_from_literal (
           0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F) (
           @repr WORDSIZE128 7) : secp256k1_field_element_t))).
-
-Definition is_infinity (p_3 : affine_t) : bool :=
-  (p_3) =.? (infinity ).
-
-Definition infinity  : affine_t :=
-  (nat_mod_one , nat_mod_zero ).
 
 Definition generator  : affine_t :=
   (
@@ -114,34 +116,6 @@ Definition neg_point (p_4 : affine_t) : affine_t :=
     p_4 in 
   (x_5, (nat_mod_zero ) -% (y_6)).
 
-Definition add_points (p_7 : affine_t) (q_8 : affine_t) : affine_t :=
-  let result_9 : (secp256k1_field_element_t × secp256k1_field_element_t) :=
-    infinity  in 
-  let '(result_9) :=
-    if is_infinity (p_7):bool then (let result_9 :=
-        q_8 in 
-      (result_9)) else (let '(result_9) :=
-        if is_infinity (q_8):bool then (let result_9 :=
-            p_7 in 
-          (result_9)) else (let '(result_9) :=
-            if (p_7) =.? (q_8):bool then (let result_9 :=
-                double_point (p_7) in 
-              (result_9)) else (let neg_q_10 : (
-                  secp256k1_field_element_t ×
-                  secp256k1_field_element_t
-                ) :=
-                neg_point (q_8) in 
-              let '(result_9) :=
-                if (p_7) =.? (neg_q_10):bool then (let result_9 :=
-                    infinity  in 
-                  (result_9)) else (let result_9 :=
-                    add_different_points (p_7) (q_8) in 
-                  (result_9)) in 
-              (result_9)) in 
-          (result_9)) in 
-      (result_9)) in 
-  result_9.
-
 Definition add_different_points
   (p_11 : affine_t)
   (q_12 : affine_t)
@@ -189,6 +163,35 @@ Definition double_point (p_21 : affine_t) : affine_t :=
       (result_22)) in 
   result_22.
 
+
+  Definition add_points (p_7 : affine_t) (q_8 : affine_t) : affine_t :=
+    let result_9 : (secp256k1_field_element_t × secp256k1_field_element_t) :=
+      infinity  in 
+    let '(result_9) :=
+      if is_infinity (p_7):bool then (let result_9 :=
+          q_8 in 
+        (result_9)) else (let '(result_9) :=
+          if is_infinity (q_8):bool then (let result_9 :=
+              p_7 in 
+            (result_9)) else (let '(result_9) :=
+              if (p_7) =.? (q_8):bool then (let result_9 :=
+                  double_point (p_7) in 
+                (result_9)) else (let neg_q_10 : (
+                    secp256k1_field_element_t ×
+                    secp256k1_field_element_t
+                  ) :=
+                  neg_point (q_8) in 
+                let '(result_9) :=
+                  if (p_7) =.? (neg_q_10):bool then (let result_9 :=
+                      infinity  in 
+                    (result_9)) else (let result_9 :=
+                      add_different_points (p_7) (q_8) in 
+                    (result_9)) in 
+                (result_9)) in 
+            (result_9)) in 
+        (result_9)) in 
+    result_9.
+
 Definition scalar_multiplication
   (k_30 : secp256k1_scalar_t)
   (p_31 : affine_t)
@@ -208,3 +211,139 @@ Definition scalar_multiplication
     q_32 in 
   q_32.
 
+Notation "p '+'' q" := (add_points p q) (at level 5, left associativity).
+
+Lemma curve_eq_symm: forall (px py qx qy : secp256k1_field_element_t),
+  (px, py) =.? (qx, qy) = (qx, qy) =.? (px, py).
+Proof.
+  intros.
+  simpl.
+  unfold nat_mod_val.
+  destruct (px =? qx) eqn:eq1. {
+    destruct (py =? qy) eqn:eq2. {
+      rewrite -> Z.eqb_sym in eq1.
+      rewrite -> Z.eqb_sym in eq2.
+      rewrite -> eq1.
+      rewrite -> eq2.
+      reflexivity.
+    } {
+      rewrite -> Z.eqb_sym in eq2.
+      rewrite -> eq2.
+      rewrite -> andb_false_r.
+      rewrite -> andb_false_r.
+      reflexivity.
+    }
+  } {
+    rewrite -> Z.eqb_sym in eq1.
+    rewrite -> eq1.
+    simpl.
+    reflexivity.
+  }
+Qed.
+
+Lemma infty_is_infty: is_infinity infinity = true.
+Proof.
+  unfold is_infinity.
+  simpl.
+  reflexivity.
+Qed.
+
+Lemma is_infty_means_infty: forall (p: affine_t), is_infinity p = true -> p =.? infinity = true.
+Proof.
+  intros p H.
+  unfold is_infinity in H.
+  apply H.
+Qed.
+
+Lemma double_neg: forall (p : affine_t), (p = neg_point (neg_point p)).
+Proof.
+  intros p.
+  unfold neg_point.
+  destruct p as (px, py).
+  assert (nat_mod_zero -% (nat_mod_zero -% py) = py). {
+    unfold "-%".
+    unfold sub.
+    unfold "nat_mod_zero - py".
+    unfold nat_mod_zero.
+    unfold "- p".
+    simpl.
+    destruct (val 115792089237316195423570985008687907853269984665640564039457584007908834671663 py) eqn:eq1. 
+    - simpl.
+      subst py.
+    Search(GZnZ.znz).
+  } {
+
+  }
+  simpl.
+
+Lemma neg_symm: forall (p q : affine_t), (neg_point p =.? q) = (p =.? neg_point q).
+Proof.
+  intros p q.
+  destruct (neg_point p =.? q) eqn:eq1. {
+    rewrite -> eqb_leibniz in eq1.
+    rewrite <- eq1.
+    Search (?a =.? ?b = true).
+  }
+
+Lemma add_infty_1: forall (p: affine_t), infinity +' p =.? p = true.
+Proof.
+  intros p.
+  unfold add_points.
+  destruct (is_infinity infinity) eqn:eq.
+  - rewrite -> Hacspec_Lib.eqb_refl.
+    + reflexivity.
+  - discriminate eq.
+Qed.
+
+Lemma add_comm: forall (p q : affine_t), add_points p q = add_points q p.
+Proof.
+  intros p q.
+  unfold add_points.
+  unfold nat_mod_val.
+  destruct (is_infinity p) eqn:eq1. {
+    destruct (is_infinity q) eqn:eq2. {
+      apply is_infty_means_infty in eq1. {
+        apply is_infty_means_infty in eq2. {
+          rewrite -> eqb_leibniz in eq1. {
+            rewrite -> eqb_leibniz in eq2. {
+              rewrite -> eq1.
+              rewrite -> eq2.
+              reflexivity.
+            }}}}} {
+              reflexivity.
+            }
+  } {
+    destruct (is_infinity q) eqn:eq2. {
+      reflexivity.
+    } {
+      destruct p as (px, py) eqn:P1.
+      destruct q as (qx, qy) eqn:Q1.
+      rewrite -> curve_eq_symm.
+      destruct ((qx, qy) =.? (px, py)) eqn:H1. {
+        assert (H2: (px, py) = (qx, qy)). {
+          Search (?a =.? ?b = true).
+          rewrite -> eqb_leibniz in H1.
+          rewrite -> H1.
+          reflexivity.
+        }
+        rewrite H2.
+        reflexivity.
+      } {
+        destruct ((px, py) =.? neg_point (qx, qy)) eqn:H2. {
+
+        }
+      }
+      (*Missing case: P = -Q*)
+      (*Missing case: P != Q /\ P != -Q /\ P != infty /\ Q != infty*)
+    }
+  }
+Qed.
+
+Lemma add_infty_2: forall (p: affine_t), p +' infinity =.? p = true.
+Proof.
+  intros p.
+  unfold add_points.
+  destruct (is_infinity p) eqn:eq1.
+  - destruct (infinity =.? p) eqn:eq2.
+    + reflexivity.
+    + apply is_infty_means_infty in eq1. 
