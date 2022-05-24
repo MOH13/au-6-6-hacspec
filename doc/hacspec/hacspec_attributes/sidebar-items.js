6a47aa5d6f6f1191feedac7f4b012afd37f3db29
Mads Overgård Henningsen <au648318@uni.au.dk>
2022-05-24 14:43:59 +0200

    Added docs

---
+++ doc/hacspec/hacspec_attributes/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"attr":[["hacspec_unsafe",""]]});
---
+++ doc/hacspec/hacspec_ecdsa_p256_sha256/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Error",""]],"fn":[["ecdsa_p256_sha256_sign",""],["ecdsa_p256_sha256_verify",""],["sign",""],["verify",""]],"type":[["PublicKey",""],["SecretKey",""],["Signature",""],["SignatureResult",""],["VerifyResult",""]]});
